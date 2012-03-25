dc256f01ae6eb55d855258d7c671638ab9556950
root <root@jadalm.jandcgroup.com>
2012-03-25 15:08:24 -0700

    setting up db.inc.php

---
+++ nomics/functions/database.inc.php
@@ -0,0 +1,10 @@
+<?php 
+    $host = "localhost";
+    $user = jamesdipadua;
+    $db_name = nomics;
+    $db_psswd = Yaxz6BfZZxtT5Uef;
+    
+    mysql_connect($host,$username, $password) OR DIE ('Unable to connect to database! Please try again later.');
+    mysql_select_db($db_name);
+    
+?>
