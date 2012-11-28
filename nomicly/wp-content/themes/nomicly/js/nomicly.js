f794765575f0c6c85f6255e5499bfdb675954d8c
jms-dipadua <james.dipadua@gmail.com>
2012-11-27 19:21:25 -0800

    rest of ajax for hot or not voting

--- nomicly/wp-content/themes/nomicly/js/nomicly.js
+++ nomicly/wp-content/themes/nomicly/js/nomicly.js
@@ -44,11 +44,23 @@ jQuery(function() {
       		chosen_idea: votedID
 		  	 }, 
  			success:  function(response){
-      					//alert(response.get_next_ideas);
-      					jQuery('.content_for_0').append(response.idea_1_consensus_percentage);
-      					jQuery('.content_for_1').append(response.idea_2_consensus_percentage);
-      					jQuery('.content_for_1').append("<br />Out of a total ").append(response.total_votes).append(" votes<br />");
-      					jQuery('#content').append(response.get_next_ideas);
+ 			// upon request success
+ 			// present the statistics to the user
+ 			// NOTE: need to create special ids/classes 
+ 			// 	to identify the "voting results" 
+ 			// 	that way it's easy after the user 'gets new ideas'
+ 			//	to drop the stats from the page (i.e. "reset the voting booth")
+      					jQuery('.content_for_0').append('<span class="idea_1_consensus">'+response.idea_1_consensus_percentage+'</span>');
+      					jQuery('.content_for_1').append('<span class="idea_2_consensus">'+response.idea_2_consensus_percentage+'</span>');
+      					
+
+      					jQuery('.content_for_1').append("<p class='total_votes'>Out of a total of "+response.total_votes+' votes</p>');
+      					
+      					jQuery('#content').append('<p class="next_ideas">'+response.get_next_ideas+'</p>');
+      					
+      					jQuery('.content_for_0 .vote-link, .content_for_1 .vote-link').hide();
+      					//todo:  instead of hiding this, remove it and then recreate it.  that would be more fool-proof
+      					
    						}
 			});// END POST
 			return false;
@@ -71,7 +83,29 @@ jQuery(function() {
 	      		action:'get_next_ideas'
       	  	 }, 
  		success:  function(response){
-      		alert(response);
+ 			// upon request success
+ 			// swap out all old content with new content
+ 			// i.e. reset the voting booth
+      			//alert(response.idea_1_data.ID);
+      			var id1 = response.idea_1_data.ID;
+      			var title1 = response.idea_1_data.post_title;
+      			var id2 = response.idea_2_data.ID;
+      			var title2 = response.idea_2_data.post_title;
+      			
+      			jQuery('.content_for_0').attr('id', id1);
+      			jQuery('.content_for_0 .vote-link').attr('id', id1);
+      			jQuery('.content_for_0 .entry-title').text(title1);
+      			jQuery('.content_for_0 #idea0').val(id1);
+      			
+      			jQuery('.content_for_1').attr('id', id2);
+      			jQuery('.content_for_1 .vote-link').attr('id', id2);
+      			jQuery('.content_for_1 .entry-title').text(title2);
+      			jQuery('.content_for_1 #idea1').val(id2);
+      			
+      			jQuery('.idea_1_consensus, .idea_2_consensus, .total_votes, .next_ideas').remove();
+      			
+      			jQuery('.content_for_0 .vote-link, .content_for_1 .vote-link ').show();
+      			
    			}
 });// END POST
 		return false;
